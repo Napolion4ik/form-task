74b428fb26e8ae4396a40932e4ebc16e68b7f8de
Napolion4ik <dziubaoleg20@gmail.com>
2022-12-19 02:57:06 +0200

    done verstka form

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".menu-icon-wrapper").onclick = function () {
+    document.querySelector(".navigation").classList.toggle("navigation-active");
+    document.querySelector(".user").classList.toggle("user-active");
+};
